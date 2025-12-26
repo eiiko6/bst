f5700a0ad1b741c38c5230260bfd9f9d415d08f4
eiiko6 <myeiiko6@gmail.com>
2025-12-26 14:34:56 +0100

    turned BST into an enum for Empty case, and implemented PartialEq

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,18 @@
 
 use std::fmt::Display;
 
-pub struct BST<T> {
-    left: Option<Box<BST<T>>>,
-    root: Option<T>,
-    right: Option<Box<BST<T>>>,
+pub enum BST<T> {
+    Empty,
+    Node {
+        left: Option<Box<BST<T>>>,
+        value: T,
+        right: Option<Box<BST<T>>>,
+    },
 }
 
 impl<T> Default for BST<T> {
     fn default() -> Self {
-        BST {
-            left: None,
-            root: None,
-            right: None,
-        }
+        BST::Empty
     }
 }
 
@@ -28,110 +27,151 @@ where
     }
 
     pub fn is_empty(&self) -> bool {
-        self.root.is_none()
+        matches!(self, BST::Empty)
     }
 
     pub fn clear(&mut self) {
-        self.root = None;
+        *self = BST::Empty
+    }
+
+    pub fn value(&self) -> Option<&T> {
+        match self {
+            BST::Empty => None,
+            BST::Node {
+                left: _,
+                value,
+                right: _,
+            } => Some(value),
+        }
     }
 
     fn left(&mut self) -> Option<&mut Self> {
-        self.left.as_deref_mut()
+        match self {
+            BST::Empty => None,
+            BST::Node {
+                left,
+                value: _,
+                right: _,
+            } => left.as_deref_mut(),
+        }
     }
 
     fn right(&mut self) -> Option<&mut Self> {
-        self.right.as_deref_mut()
+        match self {
+            BST::Empty => None,
+            BST::Node {
+                left: _,
+                value: _,
+                right,
+            } => right.as_deref_mut(),
+        }
     }
 
     pub fn count_nodes(&self) -> usize {
-        // if self.is_empty() {
-        //     return 0;
-        // }
-
-        let left_count = match &self.left {
-            Some(node) => node.count_nodes(),
-            None => 0,
-        };
-        let right_count = match &self.right {
-            Some(node) => node.count_nodes(),
-            None => 0,
-        };
-        1 + left_count + right_count
+        match self {
+            BST::Empty => 0,
+            BST::Node {
+                left,
+                value: _,
+                right,
+            } => {
+                let left_count = match &left {
+                    Some(node) => node.count_nodes(),
+                    None => 0,
+                };
+                let right_count = match &right {
+                    Some(node) => node.count_nodes(),
+                    None => 0,
+                };
+                1 + left_count + right_count
+            }
+        }
     }
 
     pub fn depth(&self) -> usize {
-        // if self.is_empty() {
-        //     return 0;
-        // }
+        match self {
+            BST::Empty => 0,
+            BST::Node {
+                left,
+                value: _,
+                right,
+            } => {
+                let left_depth = match &left {
+                    Some(node) => node.depth(),
+                    None => 0,
+                };
+                let right_depth = match &right {
+                    Some(node) => node.depth(),
+                    None => 0,
+                };
 
-        let left_depth = match &self.left {
-            Some(node) => node.depth(),
-            None => 0,
-        };
-        let right_depth = match &self.right {
-            Some(node) => node.depth(),
-            None => 0,
-        };
-
-        1 + left_depth.max(right_depth)
+                1 + left_depth.max(right_depth)
+            }
+        }
     }
 
     pub fn insert_unbalanced(&mut self, val: T) -> &mut Self
     where
         T: PartialEq + PartialOrd,
     {
-        if let Some(node) = &self.root {
-            if *node == val {
+        match self {
+            BST::Empty => {
+                *self = BST::Node {
+                    left: None,
+                    value: val,
+                    right: None,
+                };
                 return self;
             }
-            if val < *node {
-                if let Some(left_node) = self.left.as_deref_mut() {
-                    left_node.insert_unbalanced(val);
-                } else {
-                    self.left = Some(Box::new(BST {
-                        left: None,
-                        root: Some(val),
-                        right: None,
-                    }))
+            BST::Node { left, value, right } => {
+                if *value == val {
+                    return self;
                 }
-                return self;
-            } else {
-                if let Some(right_node) = self.right.as_deref_mut() {
-                    right_node.insert_unbalanced(val);
+                if val < *value {
+                    if let Some(left_node) = left.as_deref_mut() {
+                        left_node.insert_unbalanced(val);
+                    } else {
+                        *left = Some(Box::new(BST::Node {
+                            left: None,
+                            value: val,
+                            right: None,
+                        }))
+                    }
+                    return self;
                 } else {
-                    self.right = Some(Box::new(BST {
-                        left: None,
-                        root: Some(val),
-                        right: None,
-                    }))
+                    if let Some(right_node) = right.as_deref_mut() {
+                        right_node.insert_unbalanced(val);
+                    } else {
+                        *right = Some(Box::new(BST::Node {
+                            left: None,
+                            value: val,
+                            right: None,
+                        }))
+                    }
+                    return self;
                 }
-                return self;
             }
-        } else {
-            self.root = Some(val);
-            return self;
         }
     }
 
     pub fn display(&self, size: usize) {
-        // if self.is_empty() {
-        //     return;
-        // }
+        match self {
+            BST::Empty => return,
+            BST::Node { left, value, right } => {
+                if let Some(tree) = &right {
+                    tree.display(size + 1);
+                }
 
-        if let Some(tree) = &self.right {
-            tree.display(size + 1);
-        }
+                for _ in 0..size {
+                    print!("---");
+                }
 
-        for _ in 0..size {
-            print!("---");
-        }
+                println!("{}", value);
 
-        if let Some(node) = &self.root {
-            println!("{}", node);
-        }
-
-        if let Some(tree) = &self.left {
-            tree.display(size + 1);
+                if let Some(tree) = &left {
+                    tree.display(size + 1);
+                }
+            }
         }
     }
 
@@ -139,11 +179,17 @@ where
     where
         T: PartialEq + PartialOrd,
     {
-        match &self.root {
-            None => None,
-            Some(node) if *node == val => Some(self),
-            Some(node) if val < *node => self.left.as_deref()?.find(val),
-            Some(_) => self.right.as_deref()?.find(val),
+        match self {
+            BST::Empty => None,
+            BST::Node { left, value, right } => {
+                if *value == val {
+                    return Some(self);
+                } else if val < *value {
+                    return left.as_deref()?.find(val);
+                } else {
+                    right.as_deref()?.find(val)
+                }
+            }
         }
     }
 
@@ -155,29 +201,47 @@ where
     }
 
     fn left_end(&self) -> Option<&Self> {
-        if self.left.is_none() {
-            match &self.left {
-                None => Some(self),
-                Some(node) => Some(&node),
-            }
-        } else {
-            match &self.left {
-                None => Some(self),
-                Some(node) => node.left_end(),
+        match self {
+            BST::Empty => None,
+            BST::Node {
+                left,
+                value: _,
+                right: _,
+            } => {
+                if left.is_none() {
+                    match &left {
+                        None => Some(self),
+                        Some(node) => Some(&node),
+                    }
+                } else {
+                    match &left {
+                        None => Some(self),
+                        Some(node) => node.left_end(),
+                    }
+                }
             }
         }
     }
 
     fn right_end(&self) -> Option<&Self> {
-        if self.right.is_none() {
-            match &self.right {
-                None => Some(self),
-                Some(node) => Some(&node),
-            }
-        } else {
-            match &self.right {
-                None => Some(self),
-                Some(node) => node.right_end(),
+        match self {
+            BST::Empty => None,
+            BST::Node {
+                left: _,
+                value: _,
+                right,
+            } => {
+                if right.is_none() {
+                    match &right {
+                        None => Some(self),
+                        Some(node) => Some(&node),
+                    }
+                } else {
+                    match &right {
+                        None => Some(self),
+                        Some(node) => node.right_end(),
+                    }
+                }
             }
         }
     }
@@ -191,22 +255,50 @@ where
     where
         T: Clone,
     {
-        let left_clone = match &self.left {
-            None => None,
-            Some(left_tree) => Some(left_tree.clone()),
-        };
-        let right_clone = match &self.right {
-            None => None,
-            Some(right_tree) => Some(right_tree.clone()),
-        };
+        match self {
+            BST::Empty => BST::Empty,
+            BST::Node { left, value, right } => {
+                let left_clone = match &left {
+                    None => None,
+                    Some(left_tree) => Some(left_tree.clone()),
+                };
+                let right_clone = match &right {
+                    None => None,
+                    Some(right_tree) => Some(right_tree.clone()),
+                };
 
-        BST {
-            left: left_clone,
-            root: match &self.root {
-                None => None,
-                Some(value) => Some(value.clone()),
-            },
-            right: right_clone,
+                BST::Node {
+                    left: left_clone,
+                    value: value.clone(),
+                    right: right_clone,
+                }
+            }
+        }
+    }
+}
+
+impl<T> PartialEq for BST<T>
+where
+    T: PartialEq,
+    // NOTE: temporary
+    T: Display,
+{
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (BST::Empty, BST::Empty) => true,
+            (
+                BST::Node {
+                    left: l1,
+                    value: v1,
+                    right: r1,
+                },
+                BST::Node {
+                    left: l2,
+                    value: v2,
+                    right: r2,
+                },
+            ) => v1 == v2 && l1 == l2 && r1 == r2,
+            _ => false,
         }
     }
 }
@@ -226,6 +318,7 @@ mod tests {
         tree.insert_unbalanced(9);
         tree.insert_unbalanced(10);
         tree.insert_unbalanced(9);
+
         tree.display(tree.depth());
 
         assert_eq!(tree.count_nodes(), 7);
@@ -242,10 +335,12 @@ mod tests {
         tree.insert_unbalanced(9);
         tree.insert_unbalanced(7);
 
+        tree.display(tree.depth());
+
         assert!(tree.contains(2));
-        assert_eq!(tree.find(2).unwrap().root.unwrap(), 2);
-        assert_eq!(tree.find(7).unwrap().root.unwrap(), 7);
-        assert_eq!(tree.find(5).unwrap().root.unwrap(), 5);
+        assert_eq!(tree.find(2).unwrap().value().unwrap(), &2);
+        assert_eq!(tree.find(7).unwrap().value().unwrap(), &7);
+        assert_eq!(tree.find(5).unwrap().value().unwrap(), &5);
         assert!(tree.find(999).is_none());
     }
 
@@ -261,7 +356,45 @@ mod tests {
 
         tree.display(tree.depth());
 
-        assert_eq!(tree.left_end().unwrap().root.unwrap(), 2);
-        assert_eq!(tree.right_end().unwrap().root.unwrap(), 9);
+        assert_eq!(tree.left_end().unwrap().value().unwrap(), &2);
+        assert_eq!(tree.right_end().unwrap().value().unwrap(), &9);
+    }
+
+    #[test]
+    fn test_equals() {
+        let mut tree1 = BST::new();
+        tree1.insert_unbalanced(5);
+        tree1.insert_unbalanced(8);
+        tree1.insert_unbalanced(3);
+        tree1.insert_unbalanced(2);
+        tree1.insert_unbalanced(9);
+        tree1.insert_unbalanced(7);
+
+        let mut tree2 = BST::new();
+        tree2.insert_unbalanced(5);
+        tree2.insert_unbalanced(8);
+        tree2.insert_unbalanced(3);
+        tree2.insert_unbalanced(2);
+        tree2.insert_unbalanced(9);
+        tree2.insert_unbalanced(7);
+
+        assert!(tree1 == tree2);
+        tree2.insert_unbalanced(10);
+        assert!(tree1 != tree2);
+    }
+
+    #[test]
+    fn test_clone_equals() {
+        let mut tree1 = BST::new();
+        tree1.insert_unbalanced(5);
+        tree1.insert_unbalanced(8);
+        tree1.insert_unbalanced(3);
+        tree1.insert_unbalanced(2);
+        tree1.insert_unbalanced(9);
+        tree1.insert_unbalanced(7);
+
+        let tree2 = tree1.clone();
+
+        assert!(tree1 == tree2);
     }
 }
